0d9c1821b2002d9f577c45156c081e8662b4c177
spghogare456 <spghogare456@gmail.com>
2024-05-26 10:41:57 +0000

    nabar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 import './navbar.css'
 export const Navbar = () => {
   return (
-    <div className='main'>
-       
+    <div className='navbar-wrapper  flexCenter'>
+       <div className="navbar-container">
+            <ul className="nav">
+              <li><a href="#">Home</a></li>
+              <li><a href="#">About</a></li>
+              <li><a href="#">Contact</a></li>
+            </ul>
+       </div>
     </div>
   )
 }
