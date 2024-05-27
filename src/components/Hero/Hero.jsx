dc4773ebb81a43c099b9153132768b2f4737e820
sujit ghogare <spghogare456@gmail.com>
2024-05-27 10:27:14 +0000

    auth folder added

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -15,7 +15,7 @@ const Hero = () => {
         <h1>Welcome to Dragonfly</h1>
         <h3>Where Tech Meets Passion</h3>
         <div className='padding'>
-          <p>
+          <p className="paragraph">
           Welcome to the heart of innovation, Dragonfly, an electrifying club that ignites students' passion for innovation and exploration. It provides an exhilarating opportunity for students to immerse themselves in the world of technology, where they can unleash   theircreativity and expertise. From the captivating world of webdevelopment to the thrilling realm of programminglanguages,from  the cutting-edge domain of artificial intelligence to the mind-bending frontier of machine learning, Dragonfly empowers students to soar to new heights of technological prowess and ingenuity.
           </p>
         </div>
