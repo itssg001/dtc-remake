e7e46d76690e9ad35e6ee878607a608059cd2dad
sujit ghogare <spghogare456@gmail.com>
2024-06-14 07:54:26 +0000

    signuo module

---
+++ src/components/Auth/Signup.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import {Link} from 'react-router-dom';
+import Navbar from '../Navbar/Navbar';
+const Signup = () => {
+  return (
+    <>
+    <Navbar />  
+    <section className="signup">
+        <div className="signup-container">
+            SignUp
+        </div>
+    </section>
+    </>
+  )
+}
+
+export default Signup
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
+import {Link} from 'react-router-dom';
 import './navbar.css'
 export const Navbar = () => {
   return (
     <div className='navbar-wrapper  flexCenter'>
        <div className="navbar-container">
             <ul className="nav flexCenter">
-              <li><a href="#">Home</a></li>
-              <li><a href="#">About</a></li>
-              <li><a href="#">Contact</a></li>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/">About</Link></li>
+              <li><Link to="/">Contact</Link></li>
+              <li><Link to="/Signup"><button>login</button></Link></li>
             </ul>
        </div>
     </div>
